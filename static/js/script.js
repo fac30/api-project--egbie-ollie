ddea8afd39fefacd3c6760d35a245cc6c156bb04
zzhhaa <zzhhaa@protonmail.com>
2024-03-27 12:07:26 +0000

    Added comments to try / catch block for clarity

--- static/js/script.js
+++ static/js/script.js
@@ -10,17 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const searchTerm = searchInput.value; // Get the search term from the input field
         try {
+            // Make a GET request to the server with search term in route
             const response = await fetch(`/search?searchTerm=${encodeURIComponent(searchTerm)}`, {
                 method: "GET"
             });
             console.log(`response: ${response}`)
-
+            // Check if response is successful
             if (!response.ok) {
                 throw new Error("Failed to fetch joke");
             }
-
+            // Parse the JSON response to get joke data
             const data = await response.json();
-
+            // Deal with different joke structures
             if (data.type === "single") {
                 jokeElement.textContent = data.joke;
             } else if (data.type === "twopart") {
@@ -29,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 throw new Error("Unknown joke format");
             }
         } catch (error) {
+            // Catch errors that might occur during fetch or parsing 
             console.error("Error fetching joke:", error);
             jokeElement.textContent = "Sorry, we couldn't find a relevant joke!";
         }
